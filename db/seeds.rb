2940dd9795e378586e65e5d7b6b37f3f77086dcc
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 12:21:30 -0800

    Association and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{ name: 'Jennifer Beaver' }, { name: 'Taher Ali' }])
+recordings = Recording.create([{ title: 'NewRecordingForJenniferBeaver', melody: 'LaLa.wav/link', user_id: 1 },
+                                { title: 'NewRecordingForTaherAli', melody: 'DoDo.wav/link', user_id: 2 } ])
