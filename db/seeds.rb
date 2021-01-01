6e647d7095cb181d81546104d3ae616636e76137
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 17:25:49 -0800

    User model update
    Add dependent: :destroy -
    allows associated objects to be destroyed alongside this object

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#delete_all is a public activerecord method that allows us to delete specified data 
+#https://apidock.com/rails/ActiveRecord/Base/delete_all/class
 User.destroy_all
 Recording.destroy_all 
 
+jennifer = User.create(name: "Jennifer Beaver")
 
-users = User.create([{ name: 'Jennifer Beaver' }, { name: 'Taher Ali' }])
-
-recordings = Recording.create([{ title: 'NewRecordingForJenniferBeaver', melody: 'LaLa.wav/link', user_id: 1 },
-{ title: 'NewRecordingForTaherAli', melody: 'DoDo.wav/link', user_id: 2 } ])
+Jennifer_Recording1 = Recording.create(title: "NewRecordingJenniferBeaver", melody: "LaLa.wav/link", user_id: jennifer.id)
