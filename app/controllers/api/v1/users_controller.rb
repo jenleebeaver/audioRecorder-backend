49d45bbe73ce9fc79542e2b773bc165dda74d33a
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 19:19:58 -0800

    Update users_controller.rb
    Adding serializer to json in #index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
     def index
         @users = User.all
-        render json: @users
+        render json: UserSerializer.new(@users)
     end
 
     def create
