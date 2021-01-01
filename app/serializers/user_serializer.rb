d245266c32af5e9930c3d8f447c9fc3a620ad861
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 19:31:04 -0800

    Update user_serializer.rb
    adding recordings associated data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer
   #Serializer gives us a data object that contains our array with individual objects and their attributes. Nested arrays which makes it easier to access data.
   include FastJsonapi::ObjectSerializer
-  attributes :name, :created_at, :updated_at
+  attributes :name, :created_at, :updated_at, :recordings
+  #recordings allows us to access the association 
 end
