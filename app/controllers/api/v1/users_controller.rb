0eaa2cacbf54cacf8207c227c7e560f4a783ed03
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 18:09:32 -0800

    Update users_controller / create recordings_controller
    #index in users_controller creates @users instance and renders json for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :find_note, only: [:update]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
 end
