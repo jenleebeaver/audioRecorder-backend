df64779179f4a77eca88751a2ccfe650f5a5a409
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 18:40:36 -0800

    Update users_controller.rb
    #create method creating a new user with strong params and allowing us to save our new user to our json. Includes error message if we can't process the request.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #status accepted allows us to send status codes with our fetch request - accepted/rejected and why 
+            render json: @user, status: :accepted
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+            #unprocessible_entity is telling us we are unable to process instructions 
+        end
+    end
+
+    private 
+
+    def user_params
+        #strong params allow only these attributes protects our data from injection attacks 
+        params.require(:user).permit(
+            :name
+        )
+    end
 end
