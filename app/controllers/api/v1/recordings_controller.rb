96eff52706fe68a8f69b061bbf8a58bd5b9396d5
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 18:43:22 -0800

    Update recordings_controller.rb
    #create and strong_params

--- app/controllers/api/v1/recordings_controller.rb
+++ app/controllers/api/v1/recordings_controller.rb
@@ -4,4 +4,28 @@ class Api::V1::RecordingsController < ApplicationController
         @recordings = Recording.all
         render json: @recordings
     end
+
+    def create
+        @recording = Recording.new(recording_params)
+        if @recording.save
+            #status accepted allows us to send status codes with our fetch request - accepted/rejected and why 
+            render json: @recording, status: :accepted
+        else
+            render json: {errors: recording.errors.full_messages}, status: :unprocessible_entity
+            #unprocessible_entity is telling us we are unable to process instructions 
+        end
+    end
+
+    private 
+
+    def recording_params
+        #strong params allow only these attributes protects our data from injection attacks 
+        params.require(:recording).permit(
+            :title,
+            :melody,
+            :created_at,
+            :updated_at,
+            :user_id
+        )
+    end
 end
