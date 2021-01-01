6515536a52dd6d40fc7977f6811b10ca724e98a2
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 18:11:38 -0800

    Create routes for recordings

--- app/controllers/api/v1/recordings_controller.rb
+++ app/controllers/api/v1/recordings_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::RecordingsController < ApplicationController
+    before_action :find_note, only: [:update]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
 end
