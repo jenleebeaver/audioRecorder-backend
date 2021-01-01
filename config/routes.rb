8e28bf50dab38f2c2c4c7495b4c1082e0d5bb946
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-31 17:41:05 -0800

    Update routes.rb
    Create namespaced routes to associate routes with an API and version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #namespace allows us to create routes associated with an API and a version 
+
+  namespace :api do 
+    namespace :v1 do
+      resources :users, only: [:index]
+    end
+  end
 end
