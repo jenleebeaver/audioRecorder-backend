cadb2b96f99af9a3e446f47e17f6f4fa1d459557
jenleebeaver <jen.lee.beaver@gmail.com>
2021-01-01 19:07:00 -0800

    Update serializers
    Access user attributes in recordingSerializer

--- app/serializers/recording_serializer.rb
+++ app/serializers/recording_serializer.rb
@@ -1,5 +1,5 @@
 class RecordingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :melody, :created_at, :updated_at, :user_id
+  attributes :title, :melody, :created_at, :updated_at, :user_id, :user 
   #belongs_to :user --> we add this when we are using the options variable in the user#index. This should create a relationship in your json.
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   #Serializer gives us a data object that contains our array with individual objects and their attributes. Nested arrays which makes it easier to access data.
   include FastJsonapi::ObjectSerializer
-  attributes :name, :created_at, :updated_at, :recordings
+  attributes :name, :created_at, :updated_at
   #recordings allows us to access the association 
   #Note that the serializer allows associations depending on relationships
 end
