079869cfdcad022efa8891aee41ba9835a195709
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-06 14:43:24 -0700

    config/routes.rb
    add edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :users, only: [:index, :create]
-      resources :recordings, only: [:index, :create] do
+      resources :recordings, only: [:index, :create, :edit, :update] do
         get :audio, on: :member
       end
     end
