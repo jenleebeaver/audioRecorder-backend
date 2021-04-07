027c783cbd7b567938911c1a3365d40a5cceb1aa
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-07 12:35:02 -0700

    Validations
    :name, :email, :password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     #dependent destroy allows associated objects to be destroyed alongside this object 
     has_many :recordings, dependent: :destroy 
     has_secure_password 
+
+    validates :name, presence: true
+    validates :email, uniqueness: { case_sensitive: false }, presence: true
+    validates :password,  presence: true, length: {in: 6..20, messsage: "Password must be between 6 and 20 characters."} 
 end
