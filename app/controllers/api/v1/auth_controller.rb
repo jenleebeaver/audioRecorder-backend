6619fe3b82494dd1643823b4192f9639d28de4f3
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-09 13:38:23 -0700

    Add #profile

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
+    def profile
+        #using current_user helper in ApplicationController
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
     def create
         @user = User.find_by(email: user_login_params[:email])
         #authenticate comes from bcrypt
