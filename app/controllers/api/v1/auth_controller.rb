8a3a83076b0cf51e0a3ff7604130e863e04258ce
jenleebeaver <jen.lee.beaver@gmail.com>
2021-04-08 20:20:31 -0700

    auth_controller
    #create and #user_login_params

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,9 +2,19 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.find_by(email: )
+        @user = User.find_by(email: user_login_params[:email])
+        #authenticate comes from bcrypt
+        if @user && @user.authenticate(user_login_params[:password])
+            #encode token from ApplicationController
+            token = encode_token({ user_id: @user.id })
+        end
     end
 
     def user_login_params
+        #params { user: {email: 'jen.lee.beaver@gmail.com', password:'hello101'} }
+        params.require(:user).permit(
+            :email,
+            :password
+        )
     end
 end
